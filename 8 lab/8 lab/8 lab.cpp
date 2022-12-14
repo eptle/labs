a3bc525d0e312456ef02f6c416a0a03bb10fbe76
eptle <119448545+eptle@users.noreply.github.com>
2022-12-14 11:31:42 +0200

    Add files via upload

---
+++ 8 lab/8 lab/8 lab.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	unsigned short n;
+	setlocale(LC_ALL, "Rus");
+	std::cout << "Введите положительное число: " << std::endl;
+	std::cin >> n;
+	unsigned short* a = &n;
+	for (int i = 0; i < sizeof(short); i++)
+	{
+		unsigned char* b = (unsigned char*)a + i;
+		std::cout << (short)*b << "  ";
+	}
+}
