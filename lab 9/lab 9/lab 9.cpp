bce3a078b0d720d91a286c337ba0d1fb0a72e73c
eptle <119448545+eptle@users.noreply.github.com>
2022-12-25 16:32:22 +0200

    Add files via upload

---
+++ lab 9/lab 9/lab 9.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Rectangle
+{
+private:
+	int m_a = 0;
+	int m_b = 0;
+	int m_perimeter = 0;
+
+public:
+	void Perimeter(int a, int b)
+	{
+		if (a > 0 and b > 0)
+		{
+			m_a = a;
+			m_b = b;
+			m_perimeter = 2 * (m_a + m_b);
+			std::cout << "Petimeter = " << m_perimeter << std::endl;
+		}
+		else
+			std::cout << "Wrong values" << std::endl;
+	}
+};
+
+int main()
+{
+	int a, b;
+	std::cin >> a;
+	std::cin >> b;
+	Rectangle Rect1;
+	Rect1.Perimeter(a, b);
+}
